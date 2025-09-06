f7b29a114e7798210f4cdf38433bf44071a75c71
sahil kavishka <22cds0428@ms.sab.ac.lk>
2025-09-06 18:38:01 +0530

    6.37

--- petoo/models.py
+++ petoo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    ROLE_CHOICE=(
+        ("buyer","Buyer"),
+        ("seller","Seller"),
+        ("admin","admin"),
+    )
+    role=models.CharField(max_length=10,choices=ROLE_CHOICE,default="buyer")
+
+    def __str__(self):
+        return self.username
 
 # Create your models here.
